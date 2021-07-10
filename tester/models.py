e58dad335c55926e8a0028951475fc483d502bc3
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-07-09 21:15:27 -0700

    check

--- tester/models.py
+++ tester/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class ToDoList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todolist", null=True) # <--- added
     name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+	    return self.name
+
 
 class Item(models.Model):
     todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
@@ -13,4 +16,4 @@ class Item(models.Model):
     complete = models.BooleanField()
 
     def __str__(self):
-        return self.text
+	    return self.text
--- tester/views.py
+++ tester/views.py
@@ -6,27 +6,31 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
- 
-    if response.method == "POST":
-        print(response.POST)
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+
+    if ls in response.user.todolist.all():
+
+        if response.method == "POST":
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+
+                    item.save()
+
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
                 else:
-                    item.complete = False
-                    
-                item.save()
-        
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-            
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("invalid")
-                    
-    return render(response, "tester/list.html", {"ls":ls})
+                    print("invalid")
+
+
+        return render(response, "tester/list.html", {"ls":ls})
+
+    return render(response, "testr/home.html", {})
 
 
 def home(response):
@@ -37,16 +41,20 @@ def antons(response):
 
 def create(response):
     if response.method == "POST":
-        form = CreateNewList(response.POST)
+	    form = CreateNewList(response.POST)
 
-        if form.is_valid():
-            n = form.cleaned_data["name"]
-            t = ToDoList(name=n)
-            t.save()
+	    if form.is_valid():
+	        n = form.cleaned_data["name"]
+	        t = ToDoList(name=n)
+	        t.save()
+	        response.user.todolist.add(t)  # adds the to do list to the current logged in user
 
-        return HttpResponseRedirect("/%i" %t.id)
+	        return HttpResponseRedirect("/%i" %t.id)
 
     else:
         form = CreateNewList()
 
     return render(response, "tester/create.html", {"form":form})
+
+def view(response):
+    return render(response, "tester/view.html", {})
